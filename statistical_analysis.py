14e84e59d0413d12f7bb118b4f3f34a1fcad6cf6
Neofytos Neokleous <neo.neocleous@outlook.com>
2019-11-22 13:22:47 +0000

    filtering data & getting median values

--- statistical_analysis.py
+++ statistical_analysis.py
@@ -5,7 +5,6 @@ from scipy import stats
 
 data=pd.read_csv('statistic2.csv')
 data=data.dropna()
-# print(data)
 
 initial_temp = data['initial_temp']==100
 cooling_multiple = data['cooling_multiple']
@@ -14,28 +13,20 @@ num_non_improve = data['num_non_improve']
 time_taken = data['time_taken']
 kemeny_score = data['kemeny_score']
 
-# figure(figsize=(16, 8))
-# xlabel('Initial Temperature', fontsize=30)
-# ylabel('Kemeny score', fontsize=30)
-# title('Initial Temperature Vs Kemeny Score', fontsize=30)
-# scatter(initial_temp, kemeny_score, s=40, c='b', marker='o', edgecolors=None)
-#
-# line = stats.linregress(initial_temp, kemeny_score)[0]*initial_temp+stats.linregress(initial_temp, kemeny_score)[1]
-# plot(initial_temp, line,'r-', label='solid')
-# # xlim([0, 5])
-# # ylim([0, 300])
-# print ('slope=', round(stats.linregress(initial_temp, kemeny_score)[0],2))
-# print ('intercept=', round(stats.linregress(initial_temp, kemeny_score)[1], 2))
-# show()
 
+#find the min kemeny score and the average time taken to filter the data based on that
+print("Minimum kemeny score from all the data:",data.kemeny_score.min())
+print("Mean time taken from all the data:", data.time_taken.mean())
 
-print(data.kemeny_score.min())
-print(data.time_taken.mean())
-data_initialTemp_kemeny = data[['initial_temp', 'kemeny_score']]
-# data_initialTemp_kemeny.set_index['kemeny_score']
 filtering = (data.kemeny_score<70) & (data.time_taken<1.6)
 
-# all_filters = kemeny_close_to_min
+
 filtered_data = data[filtering]
-print(filtered_data)
+#shows that the num_non_improve could be as down as 2000
+print("Num_non_improve median:",filtered_data['num_non_improve'].median())
+print("Cooling multiple median:",filtered_data['cooling_multiple'].median())
+print("Temperature lenght median",filtered_data['temp_length'].median())
+
+
+filtered_data
 # data_initialTemp_kemeny
